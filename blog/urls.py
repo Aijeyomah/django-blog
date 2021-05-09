5b53d8d59e98012c1d74d162a93904f3da5fc2af
Eti Ijeoma <53005190+Aijeyomah@users.noreply.github.com>
2021-05-09 18:31:55 +0100

    django blog

---
+++ blog/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from .views import (
+    BlogListView,
+    BlogDetailView,
+    BlogCreateView,
+    BlogUpdateView,
+    BlogDeleteView,
+    post_detail # new
+)
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name="post_detail"),
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
+    path('', BlogListView.as_view(), name='home' ),
+    path('post/comment/', post_detail, name='post_comment')
+   
+]
